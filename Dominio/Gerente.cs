952eba7ef0292ee3ac769370d63cb99272c2221d
Bbrian223 <44955328+Bbrian223@users.noreply.github.com>
2025-01-20 07:13:03 -0300

    se agrega clase de acceso a datos, usuario, mozo, gerente, mesa

---
+++ Dominio/Gerente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Gerente : Usuario
+    {
+        public int IdGerente { get; set; }
+        public string Nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{   
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public bool Estado { get; set; }
+        //momentaneo
+        public string Rol { get; set; }
+
+
+        public void CambiarClave(string contra)
+        {
+            //cambio de contrasenia del usuario
+        }
+    }
+}
