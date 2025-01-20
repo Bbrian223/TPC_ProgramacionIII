212187b22c91ddd886dbdccb1839285c03d8fb48
Bbrian223 <44955328+Bbrian223@users.noreply.github.com>
2025-01-20 11:09:15 -0300

    Se agrega clases pedido, producto, venta

---
+++ Dominio/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pedido
+    {
+        public int IdPedido { get; set; }
+        public Mesa Mesa { get; set; }
+        public Usuario Empleado { get; set; }
+        public List<ItemsPedidos> ListaProductos { get; set; }
+
+    }
+
+    public class ItemsPedidos
+    {
+        public int IdItemPedidos { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public double subtotal { get; set; }
+    }
+}
---
+++ Dominio/Venta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Venta
+    {
+        public int IdVenta { get; set; }
+        public int IdPedido { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime Fecha_hora { get; set; }
+        public double Total { get; set; }
+    }
+}
